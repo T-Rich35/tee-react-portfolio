99edadd1ac01adfc7420a789dcbb1261a980700b
Tarrance <richistee@gmail.com>
2023-07-22 21:58:42 -0500

    added sidebar links

--- src/components/app.js
+++ src/components/app.js
@@ -1,15 +1,18 @@
 // import React, { Component } from "react";
-import React from "react";
+import React, { useState } from "react";
 import Topbar from "../components/topbar/Topbar";
 import Intro from "../components/intro/Intro";
 import About from "../components/about/About";
 import Projects from "../components/portjects/Portjects";
 import Contact from "../components/contact/Contact";
+import Menu from "./menu/Menu";
 
 function App() {
+  const [menuOpen, setMenuOpen] = useState(true);
   return (
     <div className="app">
-      <Topbar />
+      <Topbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+      <Menu menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
       <div className="sections">
         <Intro />
         <About />
