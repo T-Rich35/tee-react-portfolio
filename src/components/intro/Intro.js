804ef0b6b964896edacfa09a9a2f909adf656037
Tarrance <richistee@gmail.com>
2023-07-24 23:03:17 -0500

    added intro page

--- src/components/intro/Intro.js
+++ src/components/intro/Intro.js
@@ -1,5 +1,24 @@
 import React from "react";
+import { ExpandMore } from "@material-ui/icons";
 
 export default function intro() {
-  return <div className="intro" id="intro"></div>;
+  return (
+    <div className="intro" id="intro">
+      <div className="left">
+        <div className="imgContainer">
+          <img src="assets/Tee.png" />
+        </div>
+      </div>
+      <div className="right">
+        <div className="wrapper">
+          <h2>Hi, I'm</h2>
+          <h1>Tarrance </h1>
+          <span></span>
+        </div>
+        <a href="projects">
+          <ExpandMore className="down" />
+        </a>
+      </div>
+    </div>
+  );
 }
