493809efae7aed28e00c7575f6177add6c8e80c2
Tarrance <richistee@gmail.com>
2023-09-20 22:37:27 -0500

    projects page button

--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -1,36 +1,38 @@
 import React from "react";
 
-export default function Portjects() {
+export default function Porjects() {
   return (
     <div className="projects" id="projects">
-      <h1>Projects</h1>
+      <p className="pro">hover over projects to learn more</p>
       <ul>
-        <li className="active">Featured</li>
+        {/* <li className="active">Featured</li>
         <li>Blog</li>
         <li>Appointment App</li>
-        <li>Fries </li>
+        <li>Fries </li> */}
       </ul>
-      <div className="container">
-        <div className="item">
+      <div className="firstRow">
+        <div className="item1">
           <img
             src=" https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRMf4CEpg1wItoNcHURgrY14zg7vlXDyBKr4w&usqp=CAU"
             alt=""
           />
           <h3>Banking App</h3>
-          {/* <p className="par"> Mobile app build with react</p> */}
-          <div className="links">
-            <a></a>
-            <a></a>
+
+          <p className="p">
+            A simple React banking app to keep track of your daily spending
+            habits.
+          </p>
+
+          <div className="view">
+            <a href="#" target="" className="button">
+              view live
+            </a>
+            <a href="#" target="" className="button">
+              github
+            </a>
           </div>
         </div>
-        <div className="item">
-          <img
-            src=" https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRMf4CEpg1wItoNcHURgrY14zg7vlXDyBKr4w&usqp=CAU"
-            alt=""
-          />
-          <h3>Banking App</h3>
-        </div>
-        <div className="item">
+        <div className="item2">
           <img
             src=" https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRMf4CEpg1wItoNcHURgrY14zg7vlXDyBKr4w&usqp=CAU"
             alt=""
