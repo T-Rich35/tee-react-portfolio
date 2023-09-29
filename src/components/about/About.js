666b15a495167fdba1352e37947a7316e1a33fd4
Tarrance <richistee@gmail.com>
2023-09-28 20:38:24 -0500

    projects page done

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -1,5 +1,17 @@
 import React from "react";
 
 export default function About() {
-  return <div className="about" id="about"></div>;
+  return (
+    <div className="about" id="about">
+      <div className="aboutMeBio">
+        <h2> About Me</h2>
+
+        <p> things about me </p>
+
+        <p> my school </p>
+      </div>
+
+      <div className="aboutMeImg">Picture of me</div>
+    </div>
+  );
 }
