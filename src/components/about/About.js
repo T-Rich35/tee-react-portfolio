d93d8aa6cfc274fbd5edd84cf00254f227d1f69c
Tarrance <richistee@gmail.com>
2023-10-02 22:46:58 -0500

    about me work

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -4,9 +4,13 @@ export default function About() {
   return (
     <div className="about" id="about">
       <div className="aboutMeBio">
-        <h2> About Me</h2>
+        <h2 className="me"> About Me</h2>
 
-        <p> things about me </p>
+        <p>
+          Thanks for scrolling this far. I am a Full-Stack graduate from
+          <a href="#"> Bottega University</a> And from Remintgon College with a
+          Assocaite Degree in Computer Technology.
+        </p>
 
         <p> my school </p>
       </div>
