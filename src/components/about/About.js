b553b9d31028a3c5787f01933c3dcc28726b65cf
Tarrance <richistee@gmail.com>
2023-10-09 23:53:51 -0500

    about me tool kit icons

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -1,6 +1,12 @@
 import React from "react";
 import Crop from "../../../static/assets/crop.png";
 import Css from "../../../static/assets/css.png";
+import Htm from "../../../static/assets/htm.png";
+import Sass from "../../../static/assets/sass.png";
+import Java from "../../../static/assets/java.png";
+import Read from "../../../static/assets/read.png";
+import Pyt from "../../../static/assets/pyt.png";
+import Gith from "../../../static/assets/gith.png";
 
 export default function About() {
   return (
@@ -22,17 +28,46 @@ export default function About() {
 
       <div className="aboutMeImg">
         <div className="i-bg"></div>
-        <img src={Crop} alt=""></img>
-      </div>
-
-      <div className="tool">
-        {" "}
-        <h2>My Toolkit 💻</h2>
-        <ul className="skills">
-          <li>hello</li>
-          <li>2</li>
-          <li>3</li>
-        </ul>
+        <img src={Crop} alt="" className="m-img"></img>
+        <div className="tool">
+          <h2 className="m-tool">My Toolkit 💻</h2>
+          <ul className="skills">
+            <li>
+              <img className="icon" src={Htm} alt=""></img>
+              HTML5
+            </li>
+            <li>
+              <img className="icon" src={Css} alt=""></img>
+              CSS3
+            </li>
+            <li>
+              <img className="icon" src={Sass} alt=""></img>
+              Sass
+            </li>
+            <li>
+              <img className="icon" src={Java} alt=""></img>
+              Javascript
+            </li>
+            <li>
+              <img className="icon" src={Read} alt=""></img>
+              React
+            </li>
+            <li>
+              <img className="icon" src={Pyt} alt=""></img>
+              Python
+            </li>
+            <li>
+              <img className="icon" src={Gith} alt=""></img>
+              Github
+            </li>
+            {/* <li>SASS</li> */}
+            {/* <li>Javascript</li> */}
+            {/* <li>jQuery</li> */}
+            {/* <li>React</li> */}
+            {/* <li>Python</li> */}
+            {/* <li>Github</li> */}
+          </ul>
+        </div>
       </div>
     </div>
   );
