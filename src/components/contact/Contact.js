76fdfd7e69b96e44224689688529c39eb1f95ca4
Tarrance <richistee@gmail.com>
2023-10-12 00:07:57 -0500

    contact page

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -1,5 +1,21 @@
 import React from "react";
+import { LinkedInIcon } from "@material-ui/icons";
 
 export default function Contact() {
-  return <div className="contact" id="contact"></div>;
+  return (
+    <div className="contact">
+      <div className="c-wrapper">
+        <div className="c-info">
+          <h1 className="title">Let's Connect!</h1>
+          <p>
+            Just want to have a quick chat you can find me <br></br> on social
+            media or you can send me a message here!
+          </p>
+
+          <div className="socialIcons"></div>
+        </div>
+        <div className="c-form"></div>
+      </div>
+    </div>
+  );
 }
