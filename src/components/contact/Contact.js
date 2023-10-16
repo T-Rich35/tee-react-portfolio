edf8317ddc62769b82a1754b7ada44a580e1ca57
Tarrance <richistee@gmail.com>
2023-10-15 20:47:39 -0500

    contact page form

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -1,32 +1,36 @@
 import React from "react";
-import Link from "../../../static/assets/link.png";
-import Gith from "../../../static/assets/gith.png";
+import { GitHub, LinkedIn } from "@material-ui/icons";
 
 export default function Contact() {
   return (
     <div className="contact">
-      <div className="c-wrapper">
-        <div className="c-info">
-          <h1 className="title">Let's Connect!</h1>
-          <p>
-            Just want to have a quick chat you can find me <br></br> on social
-            media or you can send me a message here!
-          </p>
+      <div className="c-info">
+        <h1 className="title">Let's Connect!</h1>
+        <p>
+          Just want to have a quick chat you can find me <br></br> on social
+          media or you can send me a message here!
+        </p>
 
-          <ul className="socialIcons">
-            <li>
-              <a href="#" target="">
-                <img src={Link} alt="" className="fa"></img>
-              </a>
-            </li>
-            <li>
-              <a href="#" target="">
-                <img src={Gith} alt="" className="fa"></img>
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div className="c-form"></div>
+        <ul className="socialIcons">
+          <li>
+            <a href="#" target="">
+              <GitHub />
+            </a>
+          </li>
+          <li>
+            <a href="#" target="">
+              <LinkedIn />
+            </a>
+          </li>
+        </ul>
+      </div>
+      <div className="c-form">
+        <form>
+          <input type="text" placeholder="Name" className="n" />
+          <input type="text" placeholder="Email" className="e" />
+          <textarea placeholder="Message"></textarea>
+          <button type="submit">Send Message</button>
+        </form>
       </div>
     </div>
   );
